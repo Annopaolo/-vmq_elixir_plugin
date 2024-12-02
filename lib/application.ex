cb2d93d37e1f2d71f89f3bcc0b669e29ff4b2e87
Arnaldo Cesco <arnaldo.cesco@secomind.com>
2024-12-02 18:31:53 +0100

    Some fixes
    Signed-off-by: Arnaldo Cesco <arnaldo.cesco@secomind.com>

--- lib/application.ex
+++ lib/application.ex
@@ -5,12 +5,10 @@ defmodule VmqElixirPlugin.Application do
 
   use Application
 
-  require Logger
-
   @app_version Mix.Project.config()[:version]
 
   def start(_type, _args) do
-    Logger.info("Starting Elixir plugin v#{@app_version}.", tag: "elixir_plugin_app_start")
+    IO.puts("Starting Elixir plugin v#{@app_version}.")
 
     children = [
       # Starts a worker by calling: Aaaa.Worker.start_link(arg)
@@ -20,11 +18,11 @@ defmodule VmqElixirPlugin.Application do
     # See https://hexdocs.pm/elixir/Supervisor.html
     # for other strategies and supported options
     opts = [strategy: :one_for_one, name: VmqElixirPlugin.Supervisor]
-    Logger.info("Here too", tag: "elixir_plugin_app_start")
+    IO.puts("Here too")
 
     res = Supervisor.start_link(children, opts)
 
-    Logger.info("Finally here", tag: "elixir_plugin_app_start")
+    IO.puts("Finally here")
     res
   end
 end
